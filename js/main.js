f241896b0461f36ff654f3b10894b6cf6efdafe8
Ariana Cabana <acabana.0904@gmail.com>
2017-03-05 20:06:20 -0500

    Funcion calcularIMC

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-alert("Empieza a codear");
+function calcularIMC(){
+    var peso, altura, imc;
+    peso=document.getElementById("peso").value;
+    altura=document.getElementById("talla").value/100;
+    imc=peso/Math.pow(altura,2);
+    alert( " IMC :"  + imc.toFixed(2));
+  }
